f2430626d78ba47e9176d499b28160700bd10a22
edufrojo <edufrojo@gmail.com>
2020-07-30 19:34:12 +0200

    feat: delete plugins y medias

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var listArt = articulos;
 
-var contenedor = document.getElementById('lista-articulos');
-var articulo = document.createElement('div');
+var contenedor = document.getElementById("lista-articulos");
 
 for (let index = 0; index < listArt.length; index++) {
-    articulo.innerHTML += '<div class="col-xs-12 col-sm-6 col-md-4"><div class="articulo"><img class="img-responsive center-block" src="' + listArt[index].Imagen + '" alt="" /><h3 class="articulo__titulo text-center">' + listArt[index].Nombre + '</h3><label class="articulo__precio center-block text-center">' + listArt[index].Precio + '</label></div></div>';
-    contenedor.appendChild(articulo);
+  contenedor.innerHTML +=
+    '<div class="articulo"><img class="articulo__imagen" src="' +
+    listArt[index].Imagen +
+    '" alt="" /><h3 class="articulo__titulo">' +
+    listArt[index].Nombre +
+    '</h3><div class="articulo__precio">' +
+    listArt[index].Precio +
+    "</div></div>";
 }
